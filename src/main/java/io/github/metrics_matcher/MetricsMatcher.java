7abfc70c3277694a7ffdf4ff21de5ed3a039a343
xantorohara <xantorohara@local>
2019-02-27 10:33:07 +0300

    ui design

--- src/main/java/io/github/metrics_matcher/MetricsMatcher.java
+++ src/main/java/io/github/metrics_matcher/MetricsMatcher.java
@@ -1,6 +1,9 @@
 package io.github.metrics_matcher;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import lombok.extern.java.Log;
 
 import java.net.URL;
@@ -9,8 +12,22 @@ import java.util.ResourceBundle;
 @Log
 public class MetricsMatcher implements Initializable {
 
+    public Menu runMenu;
+    public MenuItem runMenuItem;
+    public MenuItem stopMenuItem;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
 
+    public void runAction(ActionEvent e) {
+        System.out.println("run");
+        runMenuItem.setDisable(true);
+        stopMenuItem.setDisable(false);
+    }
+
+    public void stopAction(ActionEvent e) {
+        runMenuItem.setDisable(false);
+        stopMenuItem.setDisable(true);
+    }
 }
