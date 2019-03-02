f9e1fcfd6da5cb8ced29949bd865794a6992f93b
xantorohara <xantorohara@local>
2019-03-03 00:37:21 +0300

    save

--- src/main/java/io/github/metrics_matcher/MetricsMatcher.java
+++ src/main/java/io/github/metrics_matcher/MetricsMatcher.java
@@ -5,18 +5,19 @@ import io.github.metrics_matcher.dialogs.AssetErrorDialog;
 import io.github.metrics_matcher.dialogs.NotImplementedDialog;
 import io.github.metrics_matcher.table.ResultRow;
 import javafx.application.Platform;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.css.PseudoClass;
-import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.URL;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 @Slf4j
 public class MetricsMatcher implements Initializable {
@@ -27,16 +28,28 @@ public class MetricsMatcher implements Initializable {
     public Menu metricsProfilesMenu;
     public MenuItem saveReportMenuItem;
     public Menu dataSourceMenu;
-    public MenuItem reloadMenuItem;
+    public MenuItem synchronizeMenuItem;
     public TableView<ResultRow> table;
     public TableColumn<ResultRow, String> rownumColumn;
+    public Label selectedDataSourceLabel;
+    public Tooltip selectedDataSourceTooltip;
+
+    private ToggleGroup dataSourceToggleGroup = new ToggleGroup();
+
+    public final StringProperty selectedDataSourceText = new SimpleStringProperty();
+    public final StringProperty selectedDataSourceTooltipText = new SimpleStringProperty();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        initBindings();
         synchronizeAction();
         createTable();
     }
 
+    private void initBindings() {
+        selectedDataSourceLabel.textProperty().bind(selectedDataSourceText);
+        selectedDataSourceTooltip.textProperty().bind(selectedDataSourceTooltipText);
+    }
 
     private void createTable() {
         rownumColumn.setCellFactory(column -> new TableCell<ResultRow, String>() {
@@ -104,19 +117,35 @@ public class MetricsMatcher implements Initializable {
         table.setItems(results);
     }
 
-    private DataSource selectedDataSource;
-    private MetricsProfile selectedMetricsProfile;
+    public DataSource selectedDataSource;
+    private Set<MetricsProfile> selectedMetricsProfiles = new LinkedHashSet<>();
 
     private void reloadDataSources() {
+        dataSourceMenu.getItems().clear();
         try {
             List<DataSource> dataSources = AssetsLoader.loadDataSources("configs/data-sources.json");
-            dataSources.forEach(ds -> {
-                RadioMenuItem menuItem = new RadioMenuItem(ds.getName());
-                menuItem.setOnAction(e -> selectedDataSource = ds);
+            boolean sameAsSelected = false;
+            for (DataSource dataSource : dataSources) {
+                RadioMenuItem menuItem = new RadioMenuItem(dataSource.getName());
+                menuItem.setToggleGroup(dataSourceToggleGroup);
+                menuItem.setOnAction(e -> {
+                    selectedDataSource = dataSource;
+                    selectedDataSourceText.setValue(selectedDataSource.getName());
+                    selectedDataSourceTooltipText.setValue(selectedDataSource.getUrl());
+                });
+                if (dataSource.equals(selectedDataSource)) {
+                    menuItem.setSelected(true);
+                    sameAsSelected = true;
+                }
                 dataSourceMenu.getItems().add(menuItem);
-            });
+            }
+            if (!sameAsSelected) {
+                selectedDataSource = null;
+                selectedDataSourceText.setValue("None");
+                selectedDataSourceTooltipText.setValue(null);
+            }
         } catch (AssetError e) {
-            AssetErrorDialog.show("Can't read datasources", e, "#data-sources");
+            AssetErrorDialog.show("Can't read data sources", e, "#data-sources");
         }
     }
 
@@ -126,7 +155,11 @@ public class MetricsMatcher implements Initializable {
             List<MetricsProfile> metricsProfiles = AssetsLoader.loadMetricsProfiles("configs/metrics-profiles.json");
             metricsProfiles.forEach(mp -> {
                 CheckMenuItem menuItem = new CheckMenuItem(mp.getName());
-                menuItem.setOnAction(e -> selectedMetricsProfile = mp);
+                menuItem.setOnAction(e -> {
+                    if (menuItem.isSelected()) {
+
+                    }
+                });
                 metricsProfilesMenu.getItems().add(menuItem);
             });
         } catch (AssetError e) {
@@ -150,44 +183,39 @@ public class MetricsMatcher implements Initializable {
         }
     }
 
-    public void runAction(ActionEvent e) {
-        log.debug("Run");
+    public void runAction() {
+        //todo
+        runLockMenuItems(true);
         try (Jdbc jdbc = new Jdbc()) {
             Object result = jdbc.execute(selectedDataSource, "SELECT 1 FROM DUAL");
         } catch (SQLException e1) {
             e1.printStackTrace();
         }
-
-        runMenuItem.setDisable(true);
-        stopMenuItem.setDisable(false);
-
-        saveReportMenuItem.setDisable(true);
-        metricsProfilesMenu.getItems().forEach(menuItem -> menuItem.setDisable(true));
-        dataSourceMenu.getItems().forEach(menuItem -> menuItem.setDisable(true));
     }
 
-    public void stopAction(ActionEvent e) {
-        runMenuItem.setDisable(false);
-        stopMenuItem.setDisable(true);
-
-        saveReportMenuItem.setDisable(false);
-        metricsProfilesMenu.getItems().forEach(menuItem -> menuItem.setDisable(false));
-        dataSourceMenu.getItems().forEach(menuItem -> menuItem.setDisable(false));
-
+    public final void stopAction() {
+        runLockMenuItems(false);
     }
 
-    public void showNotImplemented() {
+    private void runLockMenuItems(boolean lock) {
+        runMenuItem.setDisable(lock);
+        stopMenuItem.setDisable(!lock);
+        metricsProfilesMenu.getItems().forEach(menuItem -> menuItem.setDisable(lock));
+        dataSourceMenu.getItems().forEach(menuItem -> menuItem.setDisable(lock));
+    }
+
+    public final void showNotImplemented() {
         NotImplementedDialog.show();
     }
 
-    public void synchronizeAction() {
+    public final void synchronizeAction() {
         reloadDataSources();
         reloadMetricsProfiles();
         loadQueries();
         loadDrivers();
     }
 
-    public void exitAction() {
+    public final void exitAction() {
         Platform.exit();
     }
 }
